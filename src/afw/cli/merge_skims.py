665e36565a38603ed812fef181e8a183d65bd4f8
Nathan Nguyen <159226326+Indigo5684@users.noreply.github.com>
2026-01-13 16:48:46 -0600

    feat: split merge skim code

---
+++ src/afw/cli/merge_skims.py
@@ -0,0 +1,88 @@
+"""Command-line utility to merge skims using ``hadd``."""
+
+import logging
+import os
+import subprocess
+
+from ..objects import AnalysisConfig
+from . import utils
+
+## SOURCE: https://github.com/scikit-hep/coffea/discussions/1100
+
+
+# Merge skims together
+def merge_skims(config: AnalysisConfig, skim_dir: str) -> None:
+    """Merge multi-part skims to one file using ``hadd``
+
+    Parameters:
+        config (objects.AnalysisConfig): The config to create skims for
+        skim_dir (str): The output directory for skims
+    """
+    skim_dir_config = os.path.join(skim_dir, config.name)
+
+    # List channels
+    dirs = os.listdir(skim_dir_config)
+    merged_dir = os.path.join(skim_dir_config, "merged")
+
+    # Check for merged dir
+    if "merged" in dirs:
+        logger.critical(f"Directory {merged_dir} exists, skipping...")
+        return
+
+    # Make output dir
+    os.makedirs(merged_dir, exist_ok=True)
+
+    # Actually merge
+    for fileset in dirs:
+        # List dir, check for non-empty
+        fileset_path = os.path.join(skim_dir_config, fileset)
+        parts = [part for part in os.listdir(fileset_path) if part.endswith(".root")]
+        if len(parts) == 0:
+            logger.warning(
+                f"Skipping dir {fileset} as it doesn't contain any root files!"
+            )
+            continue
+
+        # Make target file
+        target = os.path.join(merged_dir, f"{fileset}.root")
+
+        # Run hadd
+        command = ["hadd", target] + [
+            os.path.join(fileset_path, part) for part in sorted(parts)
+        ]
+
+        logger.debug(f"Running command {command}")
+        out = subprocess.run(command)
+        if out.returncode != 0:
+            logger.critical(f"hadd returned with non-zero return code {out}")
+
+
+if __name__ == "__main__":
+    # Setup Args
+    parser = utils.get_common_args()
+    args = parser.parse_args()
+
+    # Setup Logging
+    utils.setup_logging(args.debug)
+
+    logger = logging.getLogger("Main")
+    logger.info("Loaded program")
+
+    # Create output dir
+    skim_dir = os.path.expanduser(args.skim_dir)
+    logger.info(f"Writing to skim dir {skim_dir}")
+
+    # Get Dask Client
+    client = utils.create_dask_client(args.cluster_address, [args.config])
+
+    try:
+        # Run on channel(s)
+        for config in utils.get_configs(args.config):
+            logger.info(f"Handling config {config}")
+            merge_skims(
+                config,
+                skim_dir,
+            )
+
+    finally:
+        client.close()
--- src/afw/cli/skim.py
+++ src/afw/cli/skim.py
@@ -2,7 +2,6 @@
 
 import logging
 import os
-import subprocess
 
 import awkward as ak
 import dask
@@ -166,53 +165,6 @@ def handle_config(
     if run_combined:
         logger.info("Computing all...")
         dask.compute(*to_run)
-
-
-# Merge skims together
-def merge_skims(config: AnalysisConfig, skim_dir: str) -> None:
-    """Merge multi-part skims to one file using ``hadd``
-
-    Parameters:
-        config (objects.AnalysisConfig): The config to create skims for
-        skim_dir (str): The output directory for skims
-    """
-    skim_dir_config = os.path.join(skim_dir, config.name)
-
-    # List channels
-    dirs = os.listdir(skim_dir_config)
-    merged_dir = os.path.join(skim_dir_config, "merged")
-
-    # Check for merged dir
-    if "merged" in dirs:
-        logger.critical(f"Directory {merged_dir} exists, skipping...")
-        return
-
-    # Make output dir
-    os.makedirs(merged_dir, exist_ok=True)
-
-    # Actually merge
-    for fileset in dirs:
-        # List dir, check for non-empty
-        fileset_path = os.path.join(skim_dir_config, fileset)
-        parts = [part for part in os.listdir(fileset_path) if part.endswith(".root")]
-        if len(parts) == 0:
-            logger.warning(
-                f"Skipping dir {fileset} as it doesn't contain any root files!"
-            )
-            continue
-
-        # Make target file
-        target = os.path.join(merged_dir, f"{fileset}.root")
-
-        # Run hadd
-        command = ["hadd", target] + [
-            os.path.join(fileset_path, part) for part in sorted(parts)
-        ]
-
-        logger.debug(f"Running command {command}")
-        out = subprocess.run(command)
-        if out.returncode != 0:
-            logger.critical(f"hadd returned with non-zero return code {out}")
 
 
 if __name__ == "__main__":
